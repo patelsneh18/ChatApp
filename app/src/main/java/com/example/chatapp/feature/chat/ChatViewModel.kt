1ac1ad6c6c89db61f6f08cd82bb626fc0f77d742
Sneh Patel <sneh.19bcon321@gmail.com>
2024-05-23 17:44:48 +0530

    display dates with messaged

--- app/src/main/java/com/example/chatapp/feature/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/chatapp/feature/chat/ChatViewModel.kt
@@ -12,18 +12,34 @@ import com.streamliners.base.ext.executeOnMain
 import com.streamliners.base.taskState.taskStateOf
 import com.streamliners.base.taskState.update
 import com.streamliners.base.taskState.value
+import com.streamliners.utils.DateTimeUtils
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class ChatViewModel(
     private val channelRepo: ChannelRepo,
     private val localRepo: LocalRepo
-): BaseViewModel() {
+) : BaseViewModel() {
+
+    sealed class ChatListItem {
+        class ReceivedMessage(
+            val time: String,
+            val message: Message
+        ) : ChatListItem()
+
+        class Date(val date: String) : ChatListItem()
+        class SentMessage(
+            val time: String,
+            val message: Message
+        ) : ChatListItem()
+    }
 
     class Data(
         val channel: Channel,
-        val user: User
+        val user: User,
+        val chatListItems: List<ChatListItem>
     )
+
     val data = taskStateOf<Data>()
     fun start(
         channelId: String
@@ -32,12 +48,37 @@ class ChatViewModel(
             val user = localRepo.getLoggedInUser()
             launch {
                 channelRepo.subscribeToChannel(channelId).collectLatest {
+                    val channel = channelRepo.getChannel(channelId)
                     data.update(
-                        Data(channelRepo.getChannel(channelId), user)
+                        Data(channel, user, createChatListItems(channel, user.id()))
                     )
                 }
             }
         }
+    }
+
+    fun createChatListItems(channel: Channel, currentUserId: String): List<ChatListItem> {
+        val chatItems = mutableListOf<ChatListItem>()
+        var prevDateString = ""
+        for (message in channel.messages) {
+            val dateString = DateTimeUtils.formatTime(
+                DateTimeUtils.Format.DATE_MONTH_YEAR_1,
+                message.timestamp.toDate().time
+            )
+
+            if (prevDateString != dateString) {
+                chatItems.add(ChatListItem.Date(dateString))
+                prevDateString = dateString
+            }
+
+            val chatListItem = if (message.sender == currentUserId) {
+                ChatListItem.SentMessage(message.timestamp.toString(), message)
+            } else {
+                ChatListItem.ReceivedMessage(message.timestamp.toString(), message)
+            }
+            chatItems.add(chatListItem)
+        }
+        return chatItems
     }
 
     fun sendMessage(
@@ -45,7 +86,7 @@ class ChatViewModel(
         onSuccess: () -> Unit
     ) {
         execute {
-            val message = Message (
+            val message = Message(
                 sender = data.value().user.id(),
                 message = messageStr,
                 mediaUrl = null
--- app/src/main/java/com/example/chatapp/feature/chat/comp/MessageList.kt
+++ app/src/main/java/com/example/chatapp/feature/chat/comp/MessageList.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,21 +15,42 @@ import androidx.compose.ui.unit.dp
 import com.example.chatapp.domain.ext.id
 import com.example.chatapp.domain.model.Channel
 import com.example.chatapp.feature.chat.ChatViewModel
+import com.example.chatapp.feature.chat.ChatViewModel.ChatListItem.Date
+import com.example.chatapp.feature.chat.ChatViewModel.ChatListItem.ReceivedMessage
+import com.example.chatapp.feature.chat.ChatViewModel.ChatListItem.SentMessage
 
 @Composable
 fun MessagesList(data: ChatViewModel.Data) {
-    LazyColumn (
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(12.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        items(data.channel.messages) { message ->
-            val selfSent = data.user.id() == message.sender
-            Box(modifier = Modifier.fillMaxWidth(),
-                contentAlignment = if (selfSent) Alignment.CenterEnd else Alignment.CenterStart) {
-                MessageCard(
-                    message = message
-                )
+        items(data.chatListItems) { chatListItem ->
+
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = when (chatListItem) {
+                    is SentMessage -> Alignment.CenterEnd
+                    is ReceivedMessage -> Alignment.CenterStart
+                    is Date -> Alignment.Center
+                }
+            ) {
+                when (chatListItem) {
+                    is Date -> Text(text = chatListItem.date)
+                    is ReceivedMessage -> {
+                        MessageCard(
+                            message = chatListItem.message
+                        )
+                    }
+
+                    is SentMessage -> {
+                        MessageCard(
+                            message = chatListItem.message
+                        )
+                    }
+                }
+
             }
         }
     }
